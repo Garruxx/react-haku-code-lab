26249bf9c51775bed8913f30e7d6139f99dad60d
Jhon Guerrero <garruxx@gmail.com>
2023-12-08 22:28:34 -0500

    editor.tsx use a codemirror for each code v0.0.11

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -2,6 +2,7 @@ import { CodeMirror, langs } from '../../utils/code-mirror/code-mirror'
 import styles from './style.module.sass'
 import { EditorProps } from './types'
 import { synthwave84Theme } from '../../utils/code-mirror/theme/syntwave84-theme'
+import { AcceptedLanguages } from '../../main'
 
 export const Editor = ({
 	showCode,
@@ -18,45 +19,25 @@ export const Editor = ({
 	return (
 		<div className={styles.editor}>
 			<div className={styles.code}>
-				<CodeMirror
-					className={`
-						${styles.code_mirror} 
-						${showCode == 'html' ? 'show' : ''}
-					`}
-					extensions={[langs['html']()]}
-					theme={theme}
-					basicSetup={{ tabSize: 4 }}
-					value={codes['html']}
-					readOnly={readonly?.includes(showCode)}
-					height="100%"
-					onChange={handleCodeChange}
-				/>
-				<CodeMirror
-					className={`
-						${styles.code_mirror} 
-						${showCode == 'css' ? 'show' : ''}
-					`}
-					extensions={[langs['css']()]}
-					theme={theme}
-					basicSetup={{ tabSize: 4 }}
-					value={codes['css']}
-					readOnly={readonly?.includes(showCode)}
-					height="100%"
-					onChange={handleCodeChange}
-				/>
-				<CodeMirror
-					className={`
-						${styles.code_mirror} 
-						${showCode == 'js' ? 'show' : ''}
-					`}
-					extensions={[langs['js']()]}
-					theme={theme}
-					basicSetup={{ tabSize: 4 }}
-					value={codes['js']}
-					readOnly={readonly?.includes(showCode)}
-					height="100%"
-					onChange={handleCodeChange}
-				/>
+				{Object.keys(codes).map((key) => {
+					const code = key as AcceptedLanguages
+					return (
+						<CodeMirror
+							key={key}
+							className={`
+								${styles.code_mirror} 
+								${showCode == code ? styles.show : ''}
+							`}
+							extensions={[langs[code]()]}
+							theme={theme}
+							basicSetup={{ tabSize: 4 }}
+							value={codes[code]}
+							readOnly={readonly?.includes(showCode)}
+							height="100%"
+							onChange={handleCodeChange}
+						/>
+					)
+				})}
 			</div>
 		</div>
 	)
